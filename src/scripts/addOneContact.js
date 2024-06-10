7f222b4c6c90fd6cd8be690e7f042ebb2ec20b0c
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-06-10 19:52:43 +0200

    update

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,6 +1,12 @@
 import { promises as fs } from 'fs';
-import { PATH_DB } from '../constants/contacts.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { createFakeContact } from '../utils/createFakeContact.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PATH_DB = path.join(__dirname, '../db/db.json');
 
 const addOneContact = async () => {
   try {
